bf987b2c5b0761fd15bcec91e8ce2e7cad246a49
Fabrizio Lippi <fabriziolippi86@gmail.com>
2025-04-11 13:28:20 +0200

    Fase 1 - Creazione Classe generica Libro

---
+++ Libro.java
@@ -0,0 +1,11 @@
+public class Libro<T> {
+    private T valore;
+
+    public T getValore() {
+        return valore;
+    }
+
+    public void setValore(T valore) {
+        this.valore = valore;
+    }
+}
